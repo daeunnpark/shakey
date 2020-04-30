4e68d2dfc6aabd0b384dfd4bf0c44aabaa790037
thomas <tomvetere@gmail.com>
2020-04-29 20:08:27 -0400

    Thomas - Changed vars to const, updated app name to Shakey

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var multer = require('multer');
-var cors = require('cors');
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const cors = require('cors');
 
 app.use(cors());
 
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
       destination: function (req, file, cb) {
       cb(null, 'files')
     },
@@ -14,7 +14,7 @@ var storage = multer.diskStorage({
     }
 });
 
-var upload = multer({ storage: storage }).single('file');
+const upload = multer({ storage: storage }).single('file');
 
 app.post('/upload',function(req, res) {
     upload(req, res, function (err) {

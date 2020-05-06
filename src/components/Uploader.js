a8df826ad18ca86926c31ee167f6c57afa07cbc0
daeunnpark <daeunn@gmail.com>
2020-05-06 14:31:42 -0400

    Daeun - Next button added

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -2,17 +2,25 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Uploader = () => {
-  //const [selectedFile, selectFile] = useState(null);
+  const [selectedFile, selectFile] = useState(null);
   const onChangeHandler = event => {
+    selectFile(event.target.files[0]);
+  };
+
+  const onClickHandler = event => {
     const data = new FormData();
-    data.append('file', event.target.files[0]);
+    data.append('file', selectedFile);
     axios.post("http://localhost:8000/upload", data, {})
     .then(res => {
       console.log(res.statusText);
     });
   };
 
-  return (<input type="file" name="file" onChange={onChangeHandler}/>);
+   return (
+     <div>
+       <input type="file" name="file" onChange={onChangeHandler}/>
+       <button type="button" class="btn btn-success btn-block" onClick={onClickHandler}>Next</button>
+     </div>);
 }
 
 export default Uploader;
